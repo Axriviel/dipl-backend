b9aa0e55021e4b7cacdb3c7215103f2c43a7bbb1
Axriviel <filipv01@jcu.cz>
2024-08-15 15:13:19 +0200

    add model db

---
+++ models/model.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+from models.user import User
+
+db = SQLAlchemy()
+
+class Model(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    model_name = db.Column(db.String(100), nullable=False)
+    accuracy = db.Column(db.Float, nullable=False)
+    error = db.Column(db.Float, nullable=False)
+    dataset = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    user = db.relationship('User', backref=db.backref('models', lazy=True))
+
+    def __repr__(self):
+        return f'<Model {self.model_name}>'
