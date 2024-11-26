0fd20f1400ddbef74d986d3ee4098d0d7c700d24
Axriviel <filipv01@jcu.cz>
2024-11-26 15:19:55 +0100

    add more details about model

--- models/model.py
+++ models/model.py
@@ -9,6 +9,9 @@ class Model(db.Model):
     metric_value = db.Column(db.Float, nullable=False)
     watched_metric = db.Column(db.String(100), nullable=False)
     metric_values_history = db.Column(JSON, nullable=True)  # JSON sloupec pro uložení epoch a hodnot
+    creation_config = db.Column(JSON, nullable=True) #config used to create the model
+    used_params = db.Column(JSON, nullable=True)  # used params
+    
     error = db.Column(db.Float, nullable=False)
     dataset = db.Column(db.String(100), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
